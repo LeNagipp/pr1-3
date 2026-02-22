386f8fe9ff541ebcbe52b016ea893e6f94f2a652
Maksum <maksumlubinets@gmail.com>
2026-02-22 11:02:27 +0200

    Додав фільтрацію за автором

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
-    path('contact/', views.contact, name='contact'),
+    path('<int:post_id>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,40 @@
 from django.http import HttpResponse
 
+POSTS = [
+    {'id': 1, 'title': 'Вступ до Django', 'author': 'Іван'},
+    {'id': 2, 'title': 'Робота з Views', 'author': 'Марія'},
+    {'id': 3, 'title': 'Маршрутизація в Django', 'author': 'Іван'},
+]
+
 
 def index(request):
-    return HttpResponse("Головна сторінка")
+    query_author = request.GET.get('q')
+    results = POSTS
+
+    if query_author:
+        results = [post for post in POSTS if post['author'].lower() == query_author.lower()]
+
+    output_text = "<h1>Список статей:</h1>"
+    if not results:
+        output_text += "<p>Статей цього автора не знайдено.</p>"
+
+    for post in results:
+        output_text += f"<p>ID: {post['id']} | <b>{post['title']}</b> | Автор: {post['author']}</p>"
+
+    return HttpResponse(output_text)
 
 
-def about(request):
-    return HttpResponse("Тут буде інформація про нас")
+def post_detail(request, post_id):
+    current_post = None
+    for post in POSTS:
+        if post['id'] == post_id:
+            current_post = post
+            break
 
-
-def contact(request):
-    return HttpResponse("Тут будуть наші контакти")
+    if current_post:
+        return HttpResponse(f"<h1>{current_post['title']}</h1><p>Автор: {current_post['author']}</p>")
+    else:
+        return HttpResponse("Статтю не знайдено", status=404)
 
 
 from django.shortcuts import render
