92a3c6426021c9ce6cfadfc16e9159819bce1aa9
Maksum <maksumlubinets@gmail.com>
2026-02-22 11:34:27 +0200

    Виконано 3-ю практичну роботу: додано шаблони та статику

--- blog/views.py
+++ blog/views.py
@@ -1,92 +1,45 @@
 from django.http import HttpResponse
 
+from django.shortcuts import render, redirect
+
 POSTS = [
-    {
-        'id': 1,
-        'title': 'Вступ до Django',
-        'slug': 'vstup-do-django',
-        'content': 'Django - це чудовий веб-фреймворк на Python...',
-        'category': 'python',
-        'author': 'Іван',
-        'date': '2025-01-15'
-    },
-    {
-        'id': 2,
-        'title': 'Основи Python',
-        'slug': 'osnovy-python',
-        'content': 'Python - одна з найпопулярніших мов програмування...',
-        'category': 'python',
-        'author': 'Марія',
-        'date': '2025-01-10'
-    },
-    {
-        'id': 3,
-        'title': 'HTML та CSS',
-        'slug': 'html-ta-css',
-        'content': 'HTML використовується для структури, CSS - для стилів...',
-        'category': 'web',
-        'author': 'Петро',
-        'date': '2025-01-20'
-    },
-    {
-        'id': 4,
-        'title': 'JavaScript для початківців',
-        'slug': 'javascript-dlya-pochatkivtsiv',
-        'content': 'JavaScript робить веб-сторінки інтерактивними...',
-        'category': 'web',
-        'author': 'Олена',
-        'date': '2024-12-25'
-    },
-    {
-        'id': 5,
-        'title': 'Робота з базами даних',
-        'slug': 'robota-z-bazamy-danyh',
-        'content': 'Бази даних зберігають інформацію...',
-        'category': 'database',
-        'author': 'Іван',
-        'date': '2024-11-30'
-    },
+    {'id': 1, 'title': 'Вступ до Django', 'content': '...', 'category': 'python', 'author': 'іван',
+     'date': '2025-01-15'},
+    {'id': 2, 'title': 'Основи Python', 'content': '...', 'category': 'python', 'author': 'марія',
+     'date': '2025-01-10'},
+    {'id': 3, 'title': 'HTML та CSS', 'content': '...', 'category': 'web', 'author': 'петро', 'date': '2025-01-20'},
+]
+
+
+COMMENTS = [
+    {'post_id': 1, 'author': 'Олексій', 'text': 'Дуже корисно!'},
 ]
 
 
 def index(request):
-    query_author = request.GET.get('q')
-
-    if query_author:
-        results = [p for p in POSTS if p['author'].lower() == query_author.lower()]
-    else:
-        results = POSTS
-
-    output = "<h1>Всі статті блогу</h1>"
-    if query_author:
-        output = f"<h1>Статті автора: {query_author}</h1>"
-
-    for post in results:
-        output += f"""
-            <div>
-                <h3><a href="/blog/{post['id']}/">{post['title']}</a></h3>
-                <p>Категорія: {post['category']} | Дата: {post['date']}</p>
-                <hr>
-            </div>
-        """
-    return HttpResponse(output)
+    return render(request, 'blog/index.html', {'posts': POSTS})
 
 
 def post_detail(request, post_id):
-    post = next((p for p in POSTS if p['id'] == post_id), None)
+    post = next(filter(lambda p: p['id'] == post_id, POSTS), None)
 
-    if post:
-        return HttpResponse(f"""
-            <h1>{post['title']}</h1>
-            <p><b>Автор:</b> {post['author']} | <b>Дата:</b> {post['date']}</p>
-            <p><b>Категорія:</b> {post['category']}</p>
-            <div>{post['content']}</div>
-            <br>
-            <a href="/blog/">Повернутися до списку</a>
-        """)
-    else:
-        return HttpResponse("<h2>Статтю не знайдено</h2>", status=404)
+    if post is None:
+        return render(request, 'blog/404.html', status=404)
 
+    post_comments = list(filter(lambda c: c['post_id'] == post_id, COMMENTS))
+
+    return render(request, 'blog/post_detail.html', {
+        'post': post,
+        'comments': post_comments,
+    })
+
+
+def category_posts(request, category_name):
+    posts = list(filter(lambda p: p['category'] == category_name, POSTS))
+    return render(request, 'blog/category.html', {
+        'category_name': category_name,
+        'posts': posts,
+    })
 
 from django.shortcuts import render
 
